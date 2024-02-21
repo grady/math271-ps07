9c7936435d1aea4306c8b8aa2a4310f5f87a5d17
Grady Weyenberg <gradysw@hawaii.edu>
2024-02-21 09:24:37 -1000

    updates for s24

--- tests.R
+++ tests.R
@@ -1,2 +1,4 @@
-## execute all code in the solution file
-try(knitr::knit(text=readLines('PSxx_solution.Rmd')))
+test_that("<5>Solution file exists and knits",{
+  expect_true(file.exists("solution.Rmd"))
+  suppressWarnings(expect_error(knitr::knit("solution.Rmd", quiet = TRUE, envir=globalenv()), NA))
+})
